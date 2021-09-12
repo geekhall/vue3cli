859899b9eb76b67d47a2d49104a6e64905ce686a
geekhall <174821300@qq.com>
2021-09-12 23:47:10 +0800

    20210912-234710

---
+++ 20_src_teleport/components/Child.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="child">
+      <h3>我是Child组件（子）</h3>
+      <Son/>
+  </div>
+</template>
+
+<script>
+import Son from './Son'
+
+export default {
+    name: 'Child',
+    components: {Son}
+}
+</script>
+
+<style scoped>
+    .child {
+        background-color: aqua;
+        padding: 1rem;
+    }
+</style>

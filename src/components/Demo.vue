3c24837578acb6e9e0041807f9bcaa06cb3bc2dc
geekhall <174821300@qq.com>
2021-09-12 21:59:04 +0800

    20210912-215904

--- src/components/Demo.vue
+++ src/components/Demo.vue
@@ -1,18 +1,16 @@
 <template>
+  <h2>当前求和为：{{sum}}</h2>
+  <button @click="sum++">点我++</button>
   <h2>姓名：{{name}}</h2>
   <h2>年龄：{{age}}</h2>
   <h2>薪资：{{job.j1.salary}}K</h2>
-  <h2>x.a: {{x.a}}</h2>
-  <h2>y.a: {{y.a}}</h2>
   <button @click="name+='~'">修改姓名</button>
   <button @click="age++">增长年龄</button>
   <button @click="job.j1.salary++">点我加薪</button>
-  <button @click="x.a++">点我x.a++</button>
-  <button @click="y.a++">点我y.a++</button>
 </template>
 
 <script>
-import { ref,toRefs, reactive, shallowRef} from "vue";
+import { toRefs, reactive, shallowRef} from "vue";
 
 export default {
   name: "Demo",
@@ -28,19 +26,11 @@ export default {
         }
       }
     })
-    // ref使用了reactive，生成了Proxy实现了响应式
-    let y = ref({
-      a:3
-    })
-    // shallowRef可以处理基本类型的响应式，但是不去处理对象类型的响应式（不是Proxy而是普通Object）
-    let x = shallowRef({
-      a:1
-    })
+    let sum = shallowRef(0)
 
     // 返回一个对象
     return {
-      x,
-      y,
+      sum,
       ...toRefs(person)
     }
   },

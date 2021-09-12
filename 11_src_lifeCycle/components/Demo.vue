e0314c77adffcf807206e15a937064a55f59f1eb
geekhall <174821300@qq.com>
2021-09-12 20:38:32 +0800

    20210912-203832

---
+++ 11_src_lifeCycle/components/Demo.vue
@@ -0,0 +1,79 @@
+<template>
+  <h1>当前求和为：{{ sum }}</h1>
+  <button @click="sum++">点我加一</button>
+</template>
+
+<script>
+import {
+  ref,
+  onBeforeMount,
+  onMounted,
+  onBeforeUpdate,
+  onUpdated,
+  onBeforeUnmount,
+  onUnmounted,
+} from "vue";
+
+export default {
+  name: "Demo",
+  setup() {
+    // 数据
+    let sum = ref(0);
+    onBeforeMount(()=>{
+      console.log('==== onBeforeMount ====')
+    })
+    onMounted(()=>{
+      console.log('==== onMounted ====')
+    })
+    onBeforeUpdate(()=>{
+      console.log('==== onBeforeUpdate ====')
+    })
+    onUpdated(()=>{
+      console.log('==== onUpdated ====')
+    })
+    onBeforeUnmount(()=>{
+      console.log('==== onBeforeUnmount ====')
+    })
+    onUnmounted(()=>{
+      console.log('==== onUnmounted ====')
+    })
+
+
+    // 返回一个对象
+    return {
+      sum,
+    };
+  },
+  beforeCreate() {
+    console.log("--- beforeCreate ---");
+  },
+  created() {
+    console.log("--- created ---");
+  },
+  beforeMount() {
+    console.log("--- beforeMount ---");
+  },
+  mounted() {
+    console.log("--- mounted ---");
+  },
+  beforeUpdate() {
+    console.log("--- beforeUpdate ---");
+  },
+  updated() {
+    console.log("=== updated ===");
+  },
+  beforeUnmount() {
+    console.log("=== beforeUnmount ===");
+  },
+  unmounted() {
+    console.log("=== unmounted ===");
+  },
+};
+</script>
+
+<style>
+.demo {
+  background-color: orange;
+  padding: 10px;
+}
+</style>

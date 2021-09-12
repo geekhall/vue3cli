0354454c17245a7071b1bf03d3a48947d20b53f2
geekhall <174821300@qq.com>
2021-09-12 20:45:41 +0800

    20210912-204541

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,20 @@
 <template>
   <button @click="isShowDemo = !isShowDemo">切换隐藏/显示</button>
   <Demo v-if="isShowDemo"/>
-  <Test/>
 </template>
 
 <script>
 import { ref } from 'vue';
 import Demo from './components/Demo'
-import Test from './components/Test'
 
 export default {
   name: "App",
-  components: { Demo, Test},
+  components: { Demo },
   setup(){
     let isShowDemo = ref(true)
     return{
       isShowDemo
     }
   }
-};
-
+}
 </script>
--- src/components/Demo.vue
+++ src/components/Demo.vue
@@ -1,43 +1,20 @@
 <template>
   <h1>当前求和为：{{ sum }}</h1>
   <button @click="sum++">点我加一</button>
-  <hr>
-  <h2>当前点击鼠标的坐标为：X：{{point.x}}, Y：{{point.y}}</h2>
 </template>
 
 <script>
-// import {ref ,reactive, onMounted,onBeforeUnmount} from 'vue'
 import {ref} from 'vue'
-import usePoint from '../hooks/usePoint'
 
 export default {
   name: "Demo",
   setup() {
     // 数据
     let sum = ref(0);
-    let point = usePoint()
-    // 转移至 hooks/usePoint.js
-    // let point = reactive({
-    //   x:0,
-    //   y:0
-    // })
 
-    // function savePoint(event){
-    //   point.x = event.pageX
-    //   point.y = event.pageY
-    //   console.log(event.pageX, event.pageY)
-    // }
-
-    // onMounted(()=>{
-    //   window.addEventListener('click', savePoint)
-    // })
-    // onBeforeUnmount(()=>{
-    //   window.removeEventListener('click', savePoint)
-    // })
     // 返回一个对象
     return {
-      sum,
-      point
+      sum
     }
   }
 }

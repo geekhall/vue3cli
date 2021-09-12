abf6ad53497555e50ce3e1690a38a46f53e5f00b
geekhall <174821300@qq.com>
2021-09-12 23:21:17 +0800

    20210912-232117

---
+++ 18_src_provide_inject/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="app">
+    <h3>我是App组件（爷爷）, {{name}} - {{price}}</h3>
+    <Child/>
+  </div>
+</template>
+
+<script>
+import { reactive, toRefs } from '@vue/reactivity'
+import Child from './components/Child'
+import { provide } from '@vue/runtime-core'
+
+export default {
+  name: 'App',
+  components: {Child},
+  setup(){
+    let car = reactive({
+      name: 'Benz',
+      price: '40W'
+    })
+
+    provide('car', car) // 给自己的后代组件传递数据
+    return {...toRefs(car)}
+  }
+}
+</script>
+
+<style>
+  .app {
+    background-color: grey;
+    padding: 2rem;  
+  }
+</style>
---
+++ src/components/Son.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="son">
+      <h3>我是Son组件（孙）</h3>
+      <hr>
+      <h3>{{car.name}} --- {{car.price}}</h3>
+  </div>
+</template>
+
+<script>
+import { inject } from '@vue/runtime-core'
+export default {
+    name: 'Son',
+    setup(){
+      let car = inject('car')
+      console.log(car)
+
+      return {car}
+    }
+}
+</script>
+
+<style scoped>
+    .son {
+        background-color: bisque;
+        padding: 1rem;
+    }
+</style>

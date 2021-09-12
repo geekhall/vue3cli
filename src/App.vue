d7961f2d885150109f992b6d0fe53108ca9ef9a3
geekhall <174821300@qq.com>
2021-09-13 00:28:29 +0800

    20210913-002829

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,22 @@
 <template>
   <div class="app">
     <h3>我是App组件（爷爷）</h3>
-    <Child/>
+    <Suspense>
+      <template v-slot:default>
+        <Child/>    
+      </template>
+      <template v-slot:fallback>
+        <h3>加载中。。。稍等</h3>
+      </template>
+    </Suspense>
+    
   </div>
 </template>
 
 <script>
-import Child from './components/Child'
-
+// import Child from './components/Child'   // 静态引用
+import {defineAsyncComponent} from 'vue'
+const Child = defineAsyncComponent(()=>import('./components/Child')) // 异步引入
 export default {
   name: 'App',
   components: {Child},
--- src/components/Child.vue
+++ src/components/Child.vue
@@ -1,16 +1,23 @@
 <template>
   <div class="child">
       <h3>我是Child组件（子）</h3>
-      <Son/>
   </div>
 </template>
 
 <script>
-import Son from './Son'
-
+import { ref } from '@vue/reactivity'
 export default {
     name: 'Child',
-    components: {Son}
+    async setup(){
+        let sum = ref(0)
+        let p = new Promise((resolve, reject)=>{
+            setTimeout(()=>{
+                resolve({sum})
+            }, 3000)
+        })
+
+        return await p
+    }
 }
 </script>
 
